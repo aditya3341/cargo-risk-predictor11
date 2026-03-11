f4437d90c9dca1e4478f3be9750b851657f1aea9
Aditya Dhar Dubey <dubeyadity1223@gmail.com>
2026-03-11 09:22:34 +0000

    Edit app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from model import predict_risk
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -35,4 +36,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
